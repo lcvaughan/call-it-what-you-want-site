3e57990b2a1b9705fd5867da423f9882ff1a5454
lcvaughan <lcvaughan@dons.usfca.edu>
2024-05-08 13:51:11 -0700

    imported the words and judgements page

---
+++ js/words-and-judgements.js
@@ -0,0 +1,44 @@
+var cFontRegular;
+var blahText;
+var problemsText;
+var everythingX = -50;
+var everythingY= -100;
+
+function preload() {
+  cFontRegular = loadFont('Cairo-Regular.ttf');
+  blahText = loadImage('blah-text.png');
+  problemsText = loadImage('problems-text.png');
+}
+
+function setup() {
+  createCanvas(700, 400);
+  textFont(cFontRegular);
+  textSize(20);
+  background(255);
+}
+
+function draw() {
+   noStroke()
+  fill('black');
+  text('youve taken          your words', width/5.5,height/5,width/2,height/6);
+  
+  text('and you take         your     ...',width/5.5,height/3.5,width/2,height/6);
+  
+  text('And stick them onto',width/2.7,height/1.35,width/4,height/4);
+  //text('everything',width/2.35,height/1.2,width/4,height/3);//
+  
+  image(blahText,width/6,height/2.5,width/7,height/4);
+  image(problemsText,width/1.7,height/3,width/5,height/4);
+  
+  fill(252,127,0);
+  text('everything',width/2.35,height/1.25);
+  fill(232,0,74);
+  text('judgements',everythingX,everythingY)
+  
+  fill('white');
+}
+
+function mousePressed() {
+  everythingX = mouseX;
+  everythingY = mouseY;
+}
