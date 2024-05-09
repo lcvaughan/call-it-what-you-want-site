838d2db249f8299393c091e32b6878479322301b
lcvaughan <lcvaughan@dons.usfca.edu>
2024-05-08 20:58:34 -0700

    updated rat-race page

--- js/rat-race-ladder.js
+++ js/rat-race-ladder.js
@@ -5,27 +5,27 @@ var yellowCircle2;
 var blueCircle1;
 
 function preload() {
-  cairoFont = loadFont('Cairo-Regular.ttf');
-  ratLadder = loadImage('rat-ladder.png');
-  fakeSmile = loadImage('ftp-teeth.png');
-  yellowCircle2 = loadImage('yellow-circle-2.png');
-  blueCircle1 = loadImage('blue-circle-1.png');
+  cairoFont = loadFont('img/Cairo-Regular.ttf');
+  ratLadder = loadImage('img/rat-ladder.png');
+  fakeSmile = loadImage('img/ftp-teeth.png');
+  yellowCircle2 = loadImage('img/yellow-circle-2.png');
+  blueCircle1 = loadImage('img/blue-circle-1.png');
 }
 
 function setup() {
-  createCanvas(700, 400);
+  createCanvas(windowWidth, windowHeight);
   textFont(cairoFont);
-  textSize(20);
+  textSize(40);
 }
 
 function draw() {
   background(255);
-  text('Well that',width/10,height/5);
+  text('Well that',width/8,height/5);
   text('rat race ladder-climbing',width/1.8,height/4);
   text('fake-face smiles',width/1.8,height/2.5);
-  text('got nothing on me',width/10,height/1.4);
-  text('YEAH! YEAH!',width/4,height/1.2);
-  image(ratLadder,width/3.5,height/7,width/10,height/2.5);
-  image(fakeSmile,width/1.7,height/2,width/4.5,height/6);
-  image(yellowCircle2,width/1.2,height/7,width/3,height/2);
+  text('got nothing on me',width/8,height/1.4);
+  text('YEAH! YEAH!',width/1.55,height/1.1);
+  image(ratLadder,width/7.5,height/4,width/10,height/2.5);
+  image(fakeSmile,width/1.7,height/1.5,width/4.5,height/6);
+  //image(yellowCircle2,width/1.2,height/7,width/3,height/2);//
 }
