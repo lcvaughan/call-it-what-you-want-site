347835bfe83837e27008b67d4ce56d66d80720e5
lcvaughan <lcvaughan@dons.usfca.edu>
2024-05-08 20:42:42 -0700

    Update style-and-listen.js

--- js/style-and-listen.js
+++ js/style-and-listen.js
@@ -23,13 +23,13 @@ function setup() {
 function draw() {
   background(255);
   //image(cursorImg,width/10,height/2,width/10,height/3);//
-  image(hand1,width/2.5,height/1.3,width/8,height/8);
-  image(hand2,width/1.5,height/1.3,width/8,height/8);
-  image(flameImg,width/1.95,height/2,width/6,height/3);
-  text('You say,',width/8,height/5);
-  text('"Now whats your style,',width/4,height/3);
-  text('and who do you listen to?"',width/3,height/2.5);
+  image(hand1,width/2.4,height/1.3,width/9,height/9);
+  image(hand2,width/1.6,height/1.3,width/9,height/9);
+  image(flameImg,width/1.93,height/1.7,width/9,height/4);
+  text('You say,',width/6,height/5);
+  text('"Now whats your style,',width/3,height/3);
+  text('and who do you listen to?"',width/2.5,height/2.5);
   text('WHO CARES?',width/1.95,height/1.05);
   
-  image(cursorImg, mouseX, mouseY, width/9,height/2);
+  image(cursorImg, mouseX, mouseY, width/10,height/3);
 }
